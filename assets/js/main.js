51a58fad03e8634e925fe1ea2346354c5187cba3
kashaftasawar <kashaftasawar0909@gmail.com>
2025-09-25 00:30:32 -0700

    changes

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+{/* <script>
+    var splide = new Splide('.splide', {
+        perpage:2,
+    type: "loop",
+    autoplay:true,
+    speed:2000,
+    keyboard:true,
+    rewind: true,
+    });
+
+    splide.mount();
+</script> */}
